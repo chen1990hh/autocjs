4d9e8760c5836d4c65375f0de0a1433e8dd73908
yaohaixiao <yaohaixiao@gmail.com>
2017-01-24 18:01:54 +0800

    调整API文档和gruntfile.js

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,22 +6,49 @@
             options: {
                 banner: '/* \n AutocJS - <%=pkg.description%> \n Copyright (c) 2016 yaohaixiao, all right reserved. \n homepage: <%=pkg.homepage%> \n version: <%=pkg.version%> \n author: <%=pkg.author%>  \n license: <%=pkg.license%> \n */\n'
             },
-            js: {
+            build: {
                 src: 'src/autoc.js',
                 dest: 'build/autoc.min.js'
             }
         },
         cssmin: {
-            css: {
+            options: {
+                sourceMap: true
+            },
+            build: {
                 src: 'src/autoc.css',
                 dest: 'build/autoc.min.css'
+            }
+        },
+        csslint: {
+            test: {
+                options: {
+                    "bulletproof-font-face": false,
+                    "order-alphabetical": false,
+                    "box-model": false,
+                    "vendor-prefix": false,
+                    "known-properties": false
+                },
+                src: [ 'src/*.css' ]
+            }
+        },
+        jshint: {
+            test: {
+                options: {
+                    '-W015': true
+                },
+                src: [ 'src/*.js' ],
+                filter: 'isFile'
             }
         }
     } );
 
     grunt.loadNpmTasks( 'grunt-contrib-uglify' );
     grunt.loadNpmTasks( 'grunt-contrib-cssmin' );
+    grunt.loadNpmTasks( 'grunt-contrib-csslint' );
+    grunt.loadNpmTasks( 'grunt-contrib-jshint' );
 
-    grunt.registerTask( 'default', [ 'uglify', 'cssmin' ] );
+    // 注册任务
+    grunt.registerTask( 'default', [ 'uglify', 'cssmin', 'csslint', 'jshint' ] );
 
 };
